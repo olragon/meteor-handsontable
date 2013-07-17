3347b13c023c65d1f716d8e7ffcaf7926fafc175
Long Nguyen <olragon@gmail.com>
2013-07-17 16:38:21 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Handsontable repackaged for Meteor
+=========
+
+[Handsontable](http://handsontable.com/) is a minimalistic Excel-like data grid editor for HTML, JavaScript & jQuery.
+
+Version: 0.9.9
---
+++ package.js
@@ -0,0 +1,8 @@
+Package.describe({
+  summary: "Handsontable is a minimalistic approach to Excel-like table editor in HTML & jQuery"
+});
+
+Package.on_use(function (api, where) {
+  api.use('jquery', 'client');
+  api.add_files(['lib/jquery.handsontable.full.js', 'lib/jquery.handsontable.full.css'], 'client');
+});
